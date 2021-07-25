f1b6d5b5b3709881edf2a997d263287765e46817
dohun <37826908+shine-17@users.noreply.github.com>
2021-07-25 23:51:19 +0900

    Update Thread.java

--- java/Thread.java
+++ java/Thread.java
@@ -1,15 +1,10 @@
 package app;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
-class Task implements Runnable {
-	
-	public Long ipToLong(String ip) {
+public class PingCheck  {
+	// String -> Long
+	public static Long ipToLong(String ip) {
 		String[] ipArr = ip.split("\\.");
 		long num = 0;
 		
@@ -17,82 +12,57 @@ class Task implements Runnable {
 			int power = 3-i;
 			num += Integer.parseInt(ipArr[i])%256 * Math.pow(256, power);
 		}
-		
 		return num;
 	}
-	
-	public static String intToIp(int i) {
+	// long -> String
+	public static String intToIp(long i) {
 		return ((i >> 24) & 0xFF) + "." + ((i >> 16) & 0xFF) + "." + ((i >> 8) & 0xFF) + "." + (i & 0xFF);
 	}
-	
-	public synchronized void run() {
-		InetAddress pingcheck = null;
-		boolean isAlive = false;
-		int count = 0;
+		
+	public static void main(String[] args) throws IOException {
+		String startIp = "172.30.1.1";
+		String endIp = "172.30.1.100";
+		Long start = ipToLong(startIp);
+		Long end = ipToLong(endIp);
+		long ipLength = end - start+1;
 		
 		try {
-			Thread.sleep(1000);
+			MyThread.pingCount = 0;
+			MyThread.count = 0;
+			int threadCount = 20;
 			
-			//String endIp = br.readLine();
-			String startIp = "172.30.1.1";
-			String endIp = "172.30.1.10";
-			Long ip1 = ipToLong(startIp);
-			Long ip2 = ipToLong(endIp);
-			
-			System.out.println("startIp : "+ ip1);
-			System.out.println("endIp : "+ intToIp(ip2.intValue()));
+			MyThread[] threads = new MyThread[threadCount];
 			
 			long beforeTime = System.currentTimeMillis();
-			for(long i=ip1; i<=ip2; i++) {
-				Long l = i;
-				pingcheck = InetAddress.getByName(intToIp(l.intValue()));
-				isAlive = pingcheck.isReachable(1000);
-				System.out.println("ip : "+pingcheck + "," + isAlive);
-				System.out.println("ip : "+intToIp(l.intValue()) + " / Thread : "+Thread.currentThread().getName() + " / 성공여부 : "+isAlive);
+			
+			for(int i=0; i<threads.length; i++) {
+				long a = 0;
+				long b = 0;
 				
-				if(isAlive == true)
-					count++;
+				if(ipLength % threadCount == 0 ) {
+					a = start+i*(ipLength/threadCount);
+					b = end-(threadCount-(i+1))*(ipLength/threadCount);
+				} else {
+					a = start+i*(ipLength/threadCount);
+					b = a + (ipLength/threadCount-1);
+					
+					if(i == threads.length-1)
+						b = end;
+				}
+				threads[i] = new MyThread(a, b);
+				
+				threads[i].start();
+			}
+			for(int i=0; i<threads.length; i++) {
+				threads[i].join();
 			}
 			long afterTime = System.currentTimeMillis();
 			long secDiffTime = (afterTime - beforeTime); //두 시간에 차 계산
-			System.out.println("개수 : "+(ip2 - ip1));
-			System.out.println("실행시간 : " + secDiffTime + " / 응답핑 수 : " + count);
-			
+			System.out.println("응답핑 : " + MyThread.pingCount + " / 연산시간 : "+secDiffTime + "ms");
+			System.out.println("연산개수 : "+MyThread.count);
 		} catch (Exception e) {
-			e.printStackTrace();
+			
 		}
-	}
-}
-
-public class PingCheck  {
-	public static void main(String[] args) throws IOException {
-		BlockingQueue<Runnable> blockingQueue = new ArrayBlockingQueue<>(1); 
-//		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1, TimeUnit.SECONDS, blockingQueue);
-		
-		Runnable task = new Task();
-		Thread thread1 = new Thread(task);
-		Thread thread2 = new Thread(task);
-		Thread thread3 = new Thread(task);
-		Thread thread4 = new Thread(task);
-		Thread thread5 = new Thread(task);
-		Thread thread6 = new Thread(task);
-		Thread thread7 = new Thread(task);
-		Thread thread8 = new Thread(task);
-		Thread thread9 = new Thread(task);
-		Thread thread10 = new Thread(task);
-		
-		thread1.start();
-		thread2.start();
-		thread3.start();
-		thread4.start();
-		thread5.start();
-		thread6.start();
-		thread7.start();
-		thread8.start();
-		thread9.start();
-		thread10.start();
-		
-//		threadPoolExecutor.execute(task);
 		
 	}
 
