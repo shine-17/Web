a02758fbcc2974c3b978b811dddb5818fc4768ca
dohun <37826908+shine-17@users.noreply.github.com>
2022-04-18 23:22:09 +0900

    Update FAP_Recursion.java

--- java/FAP_Recursion.java
+++ java/FAP_Recursion.java
@@ -6,43 +6,47 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.zip.CRC32;
 import java.util.zip.CheckedInputStream;
 
 public class FileSearch {
-	private static CheckedInputStream in = null;
 	private static HashMap<File, Object> fileMap = new HashMap<File, Object>();
-	private static CRC32 crc32 = new CRC32();
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String filePath = "C:\\fap\\";
 		searchFile(filePath); //최초 전체파일 탐색 후 CRC값 저장
 		
-		String filePath_1 = br.readLine();
+//		String filePath_1 = br.readLine();
+		
+		System.out.println("---------------------");
 		
 		Iterator<File> iter = fileMap.keySet().iterator();
+		
 		while(iter.hasNext()) {
 			File item = iter.next();
 //			System.out.println(item.getPath() + " : " + fileMap.get(item));
 			
-			in = new CheckedInputStream(new FileInputStream(item.getPath()), crc32);
-			long crcValue = -1;
-			while (in.read() != -1)
-			crcValue = crc32.getValue();
+			long crcValue = getFileCRC32(item.getPath());
 			
-			System.out.println(item.getName() + " : " + fileMap.get(item));
-			System.out.println(item.getName() + " : " + crcValue);
+//			System.out.println(item.getPath());
+			System.out.println(crcValue);
+			
+//			System.out.println(item.getName() + " : " + fileMap.get(item));
+//			System.out.println(item.getName() + " : " + crcValue);
 			
 			if((long)fileMap.get(item) == crcValue) {
-				System.out.println(item.getName() + " : 이상없음");
+//				System.out.println(item.getName() + " : 이상없음");
 			} else if((long)fileMap.get(item) != crcValue) {
-				System.out.println("수정");
+//				System.out.println("수정");
 			} else {
-				System.out.println("삭제");
+//				System.out.println("삭제");
 			}
 				
 		}
@@ -57,18 +61,39 @@ public class FileSearch {
 		File[] fList = path.listFiles();
 		
 		for(int i=0; i<fList.length; i++) {
-			
 			if(fList[i].isFile()) { // 상위 File
-				in = new CheckedInputStream(new FileInputStream(fList[i].getPath()), crc32);
-				long crcValue = -1;
-				while (in.read() != -1);
-				crcValue = crc32.getValue();
-				
+				long crcValue = getFileCRC32(fList[i].getPath());
+				System.out.println(crcValue);
 				fileMap.put(fList[i], crcValue);
 			} else if(fList[i].isDirectory()) { // 디렉토리 하위 File
 				searchFile(fList[i].getPath());
 			}
 		}
+	}
+	
+	private static long getFileCRC32(String filePath) throws IOException {
+		long crcValue = -1;   
+	    CRC32 crc32 = new CRC32();   
+	    CheckedInputStream in = null;
+	    
+	    try {
+	    	in = new CheckedInputStream(new FileInputStream(filePath), crc32);
+	    	
+			while (in.read() != -1)
+				crcValue = crc32.getValue();
+		} catch (FileNotFoundException e) {   
+	        System.err.println("CheckedIODemo: " + e);   
+	        System.exit(-1);   
+	    } catch (IOException e) {   
+	        System.err.println("CheckedIODemo: " + e);   
+	        System.exit(-1);   
+	    } finally {
+	        if(in != null) {
+	            in.close();
+	        }
+	    }
+		
+		return crcValue;
 	}
 
 }
