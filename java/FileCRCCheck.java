d856df71c5108902c0e7ff88d51080c7ad54a445
dohun <37826908+shine-17@users.noreply.github.com>
2022-04-28 00:33:35 +0900

    Add files via upload

---
+++ java/FileCRCCheck.java
@@ -0,0 +1,63 @@
+package Task;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.zip.CRC32;
+
+public class FileCRCCheck {
+	private static HashMap<File, Object> fileMap = new HashMap<File, Object>();
+	private static BufferedReader br;
+	private static Iterator<File> iter;
+	
+	public static void searchFile(String filePath) throws IOException {
+		File path = new File(filePath);
+		File[] fList = path.listFiles();
+		
+		for(int i=0; i<fList.length; i++) {
+			if(fList[i].isFile()) { // File
+				long crcValue = getFileCRC32(fList[i].getPath());
+				fileMap.put(fList[i], crcValue);
+			} else if(fList[i].isDirectory()) { // Directory
+				searchFile(fList[i].getPath());
+			}
+		}
+	}
+
+	
+	// 파일 CRC 값
+	public static long getFileCRC32(String filePath) throws IOException {
+		long crcValue = -1;   
+	    CRC32 crc32 = new CRC32();   
+	    BufferedInputStream in = null;
+	    
+	    try {
+	    	in = new BufferedInputStream(new FileInputStream(filePath));
+	    	byte buffer[] = new byte[32768];
+	    	int length = 0;
+	    	
+	    	while ((length = in.read(buffer)) >= 0)
+	    		crc32.update(buffer, 0, length);
+	    	
+			crcValue = crc32.getValue();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+	        System.exit(-1);   
+	    } catch (IOException e) {   
+	    	e.printStackTrace();
+	        System.exit(-1);   
+	    } finally {
+	        if(in != null) {
+	            in.close();
+	        }
+	    }
+		
+		return crcValue;
+	}
+		
+}
